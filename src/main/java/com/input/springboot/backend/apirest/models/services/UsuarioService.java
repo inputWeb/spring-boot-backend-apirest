0e504ad3ef97197d31ad042cce711f060baef67e
sergi <sergi.gim@gmail.com>
2019-01-18 00:27:21 +0100

    UsuarioService + UsuarioDao
    Service comprueba los users y los roles que tienen
    Dao es un metodo para buscar el usuario en la BD (hay dos maneras de hacer el metodo)

---
+++ src/main/java/com/input/springboot/backend/apirest/models/services/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.input.springboot.backend.apirest.models.services;
+
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.GrantedAuthoritiesContainer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.input.springboot.backend.apirest.models.dao.IUsuarioDao;
+import com.input.springboot.backend.apirest.models.entity.Usuario;
+
+@Service 
+public class UsuarioService implements UserDetailsService{
+	
+	private Logger logger = LoggerFactory.getLogger(UsuarioService.class);		//permite crear logs
+	
+	@Autowired
+	private IUsuarioDao usuarioDao;
+	
+	@Override
+	@Transactional(readOnly=true)
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario usuario = usuarioDao.findByUsername(username);	//Recuperamos el usuario que queramos comprobar
+		
+		if(usuario == null) {
+			logger.error("Error en el login: no existe el usuario '"+ username +"' en el sistema");		//log de error guardado
+			throw new UsernameNotFoundException("Error en el login: no existe el usuario '"+ username +"' en el sistema"); //hacemos saltar la excepcion con el error
+		}else {
+			
+		}
+		
+		List<GrantedAuthority> authorities = usuario.getRoles()		//obtenemos todos los roles del usuario
+				.stream()		//separamos los roles en bloques
+				.map(role -> new SimpleGrantedAuthority(role.getNombre())) //hay que buscar los permisos del usuario pero hay que maperlo al objeto GrantedAuthority
+				.peek(authority -> logger.info("Role: " + authority.getAuthority())) // ver el nombre del role en la consola
+				.collect(Collectors.toList());			//hay que convertilo tmb a una lista para que coincida con los requerimientos
+		
+				
+		
+		return new User(usuario.getUsername(), usuario.getPassword(), usuario.getEnabled(), true, true, true, authorities);
+	}
+
+}
